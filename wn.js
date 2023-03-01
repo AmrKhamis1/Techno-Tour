1b068d0167f0915453c1f7521ce1a02129955173
Amr Khamis <101368138+AmrGraphics@users.noreply.github.com>
2023-03-01 20:37:58 +0200

    Delete wn.js

--- wn.js
+++
@@ -1,8 +0,0 @@
-let myVar = setInterval(sen ,0);
-      function  sen(){
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onload = function() {
-      document.getElementById("txtHint").innerHTML = this.responseText;
-    }
-  xmlhttp.open("POST","masg.txt");
-  xmlhttp.send();}
