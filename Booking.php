84db7092f22f52ef1556943be05566cf18eaee98
ahmedhosni1 <93822114+ahmedhosni1@users.noreply.github.com>
2023-03-09 20:46:55 +0200

    Create Booking.php

---
+++ Booking.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Booking.css">
+    <title>Rooms Booking</title>
+</head>
+<body>
+<div id="container">
+    </div>
+    <form  id="form22" action="Booking.php" method="post" enctype="multipart/form-data">
+                <div class="a">
+                    <h1>Rooms Booking</h1>
+                    <label for="b_title">Booking Title</label>
+                    <input id="b_title"  type="text" required>
+
+                    <label for="b_date">Date</label >
+                    <input id="b_date" type="date" required>
+
+                    <label for="b_start">Start Time</label>
+                    <input id="b_start" type="time" required>
+
+                    <label for="b_end">End Time</label>
+                    <input id="b_end" type="time" required>
+
+                    <label for="b_duration">Duration</label>
+                    <input id="b_duration" type="number" required>
+                </div>
+
+                <div class="b">
+                    <label for="b_attendees">Total number of attendees</label>
+                    <input id="b_attendees" type="number" required>
+
+                    <label for="b_external_attendees">Number of External Attendees</label>
+                    <input id="b_external_attendees" type="number" required>
+
+                    <label for="b_Num_rooms">Number of rooms</label>
+                    <input id="b_Num_rooms" type="number" required>
+                    <br>
+                    <br>
+                    <input type="submit" value="Done" id="done" class="done">
+        </div>
+</body>
+</html>
