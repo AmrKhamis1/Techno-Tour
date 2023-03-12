aeea9e3c55cf0c05287b23db0fa95a6ab729a4a0
Amr Khamis <101368138+AmrKhamis1@users.noreply.github.com>
2023-03-12 14:40:19 +0200

    Create database.php

---
+++ mempers/database.php
@@ -0,0 +1,18 @@
+<?php
+// Create connection to database 
+//main file 
+
+//host=localhost -->php my admin
+//database name amrdb
+//charset=utf8
+//name = root
+//password=""
+$conn = new PDO("mysql:host=localhost;dbname=technotour;charset=utf8;", "root","");
+// Check connection
+if ($conn) {
+    echo "";
+}else{
+    echo "not connect";
+}
+
+?>
