19e24f1e866ea68754a67d1ec4608ce8ccb316c1
Adel-Sherif <os6225091@gmail.com>
2023-03-14 17:07:57 +0200

    Creata About us Page

--- about us.php
+++ about us.php
@@ -1,44 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/about us.css">
-    <title>About Us</title>
- 
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="CSS/about_us.css">
+  <title>About Us</title>
 </head>
 <body>
-    <center><h1 id="user"></h1></center>
-    <center><div class="team">
-         <h2>Project team</h2><br>
-    <div class="team-main">
-      <div class="team-list">
-         <img src="Team photos/Photo1.jpg" loading="lazy" alt="">
-         <div class="team-text">
-             <h3>Amr Khamis</h3>
-         </div>
-         
+<div class="container">
+  </div>
+    <header id="header-style">
+      <a href="index.php"><img class="logo-img" src="techno tour website design pro\web site logo2.png" alt="LOGO"></a>
+      <ul >
+         <li><a title="home page" href="index.php" class="links">Home</a></li>
+         <li><a title="countact us" href="contact us.php" class="links">Contact Us</a></li>
+         <li><a title="about us" href="about us.php" class="links">About Us</a> </li>
+         <li><a title="what's new" href="what's new.php" class="links">What's New</a></li>
+      </ul>
+      <div class="search-logo"><input class="search" type="text">Search <img class="search-logo-img" src="techno tour website design pro\search.png" alt="..">
       </div>
-      <div class="team-list">
-        <img src="Team photos/Photo1.jpg" loading="lazy" alt="">
-        <div class="team-text">
-          <h3>Shahd Abo Elkaramat</h3>
-        </div>
+      <div class="user-logo"><a href="form.php" id="login-logo" style="text-decoration: none;color: #000756; margin-right:5px ;">
+      demo
+      </a><img class="login-logo-img" src="techno tour website design pro\login.png" alt=".."></div>
+   </header>
+    <div class="aboutus" id="aboutus">
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti perspiciatis cumque delectus obcaecati earum unde quasi accusamus consequatur ducimus blanditiis,
+         facilis nesciunt aut iure magni amet officia ipsum pariatur voluptatibus.</p>
+      <button type="button"  id="contactus">
+        <a href="contact us.php">Contact Us Now</a>
+      </button>
+    </div>
+    <div class="team">
+      <h1>Our Team</h1>
+      <div class="adel">
+        <img src="techno tour website design pro/adel.jpg" alt="Adel">
+        <p>Adel Sherif</p>
       </div>
-      <div class="team-list">
-        <img src="Team photos/Photo1.jpg" loading="lazy" alt="">
-        <div class="team-text">
-          <h3>Sara Magdy</h3>
-        </div>
+      <div class="amr">
+        <img src="techno tour website design pro/amr.jpg" alt="Amr">
+        <p>Amr Khames</p>
       </div>
-      <div class="team-list">
-        <img src="Team photos/Photo1.jpg" loading="lazy" alt="">
-        <div class="team-text">
-            <h3>Ahmed Hosny</h3>
-        </div>
+      <div class="ahmed">
+        <img src="techno tour website design pro/ahmed.jpg" alt="Ahmed">
+        <p>Ahmed Hosny</p>
+      </div>
+      <div class="sara">
+        <img src="techno tour website design pro/sara.jpg" alt="Sara">
+        <p>Sara Magdy</p>
+      </div>
+      <div class="shahd">
+        <img src="techno tour website design pro/shahd.jpg" alt="Shahd">
+        <p>Shahd Abo Elkaramat</p>
       </div>
     </div>
-    </div></center>
 </body>
 </html>
