7608100ba7a7e2776976de2df82251ab63a7929b
Adel-Sherif <os6225091@gmail.com>
2023-03-26 17:43:33 +0200

    edits

--- about us.php
+++ about us.php
@@ -8,8 +8,10 @@
   <title>About Us</title>
 </head>
 <body>
-<div class="container">
-  </div>
+<section><!---That Main Section Have All Page--->
+  <!--------------Start Page Container----------------->
+  <div class="container" id="container">
+    <!--------------Start Header----------------->
     <header id="header-style">
       <a href="index.php"><img class="logo-img" src="techno tour website design pro\web site logo2.png" alt="LOGO"></a>
       <ul >
@@ -24,6 +26,8 @@
       demo
       </a><img class="login-logo-img" src="techno tour website design pro\login.png" alt=".."></div>
    </header>
+   <!--------------End Header----------------->
+   <!--------------Start About us contain----------------->
     <div class="aboutus" id="aboutus">
       <h1>About Us</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti perspiciatis cumque delectus obcaecati earum unde quasi accusamus consequatur ducimus blanditiis,
@@ -32,28 +36,378 @@
         <a href="contact us.php">Contact Us Now</a>
       </button>
     </div>
+       <!--------------End About us contain----------------->
+   <!--------------Start Team contain----------------->
     <div class="team">
-      <h1>Our Team</h1>
-      <div class="adel">
+      <h1>Techno-Tour Team</h1>
+      <div class="adel" id="adel">
         <img src="techno tour website design pro/adel.jpg" alt="Adel">
-        <p>Adel Sherif</p>
+        <h2>Adel Sherif</h2>
       </div>
-      <div class="amr">
-        <img src="techno tour website design pro/amr.jpg" alt="Amr">
-        <p>Amr Khames</p>
+      <div class="amr" id="amr">
+        <img src="techno tour website design pro/adel.jpg" alt="Amr">
+        <h2>Amr Khames</h2>
       </div>
-      <div class="ahmed">
-        <img src="techno tour website design pro/ahmed.jpg" alt="Ahmed">
-        <p>Ahmed Hosny</p>
+      <div class="ahmed" id="ahmed">
+        <img src="techno tour website design pro/adel.jpg" alt="Ahmed">
+        <h2>Ahmed Hosny</h2>
       </div>
-      <div class="sara">
-        <img src="techno tour website design pro/sara.jpg" alt="Sara">
-        <p>Sara Magdy</p>
+      <div class="sara" id ="sara">
+        <img src="techno tour website design pro/adel.jpg" alt="Sara">
+        <h2>Sara Magdy</h2>
       </div>
-      <div class="shahd">
-        <img src="techno tour website design pro/shahd.jpg" alt="Shahd">
-        <p>Shahd Abo Elkaramat</p>
+      <div class="shahd" id="shahd">
+        <img src="techno tour website design pro/adel.jpg" alt="Shahd">
+        <h2>Shahd Abo Elkaramat</h2>
       </div>
     </div>
+       <!--------------Start Team contain----------------->
+  </div>
+    <!--------------End Page Container----------------->
+
+                    <!-------------------------------pop up pages----------------------------->
+
+<!------------Start Adel popup---------->
+<div id="popup-adel-body">
+  <img id ="close" src="techno tour website design pro/exit.png" alt="">
+  <center><div class="popup-container">
+    <span id="image"><img src="techno tour website design pro/adel.jpg" alt="adel"></span>
+    <h2 id="popup-name">Adel Sherif Adel</h2>
+    <!-----------Skills----------->
+    <span id="popup-skills">
+      <span id="popup-title-Skills">Skills</span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Html :</span> 
+        <span id="popup-subject-body"><span id="popup-adel-html">100%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Css :</span> 
+        <span id="popup-subject-body"><span id="popup-adel-css">90%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">C++ :</span> 
+        <span id="popup-subject-body"><span id="popup-adel-c">70%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">JS :</span> 
+        <span id="popup-subject-body"><span id="popup-adel-js">60%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Python :</span> 
+        <span id="popup-subject-body"><span id="popup-adel-python">100%</span></span>
+      </span>
+    </span>
+    <!-----------Contact----------->
+    <span id="popup-contact">
+      <span id="popup-contact-title">Contact Me</span> 
+      <span id="popup-contact-row">
+        <span id="popup-adel-whatsapp"><img src="techno tour website design pro/whatsapp.png" alt="">Whatsapp: </span>
+        <p><a href="https://whatsapp.com/dl/">01283048731</a></p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-adel-facebook"><img src="techno tour website design pro/facebook.png" alt="">Facebook: </span>
+        <p><a href="https://www.facebook.com/profile.php?id=100008321981890">Adel Sherif</a> </p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-adel-phone"><img src="techno tour website design pro/phone.png" alt="">Phone: </span>
+        <p>01283048731</p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-adel-gmail"><img src="techno tour website design pro/gmail.png" alt="">Gmail: </span>
+        <p><a href="https://mail.google.com/mail/u/0/#inbox">Os6225091@Gmail.com</a> </p>
+      </span>
+    </span>
+  </div></center>
+</div>
+<!------------ End Adel popup---------->
+
+<!------------Start Amr popup---------->
+<div id="popup-amr-body">
+  <img id ="close" src="techno tour website design pro/exit.png" alt="">
+  <center><div class="popup-container">
+    <span id="image"><img src="techno tour website design pro/adel.jpg" alt="adel"></span>
+    <h2 id="popup-name">Amr Khames</h2>
+    <!-----------Skills----------->
+    <span id="popup-skills">
+      <span id="popup-title-Skills">Skills</span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Html :</span> 
+        <span id="popup-subject-body"><span id="popup-amr-html">100%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Css :</span> 
+        <span id="popup-subject-body"><span id="popup-amr-css">90%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">C++ :</span> 
+        <span id="popup-subject-body"><span id="popup-amr-c">70%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">JS :</span> 
+        <span id="popup-subject-body"><span id="popup-amr-js">60%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Python :</span> 
+        <span id="popup-subject-body"><span id="popup-amr-python">100%</span></span>
+      </span>
+    </span>
+    <!-----------Contact----------->
+    <span id="popup-contact">
+      <span id="popup-contact-title">Contact Me</span> 
+      <span id="popup-contact-row">
+        <span id="popup-amr-whatsapp"><img src="techno tour website design pro/whatsapp.png" alt="">Whatsapp: </span>
+        <p><a href="https://whatsapp.com/dl/">01283048731</a></p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-amr-facebook"><img src="techno tour website design pro/facebook.png" alt="">Facebook: </span>
+        <p><a href="https://www.facebook.com/profile.php?id=100008321981890">Adel Sherif</a> </p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-amr-phone"><img src="techno tour website design pro/phone.png" alt="">Phone: </span>
+        <p>01283048731</p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-amr-gmail"><img src="techno tour website design pro/gmail.png" alt="">Gmail: </span>
+        <p><a href="https://mail.google.com/mail/u/0/#inbox">Os6225091@Gmail.com</a> </p>
+      </span>
+    </span>
+  </div></center>
+</div>
+<!------------ End Amr popup---------->
+
+<!------------ Start Ahmed popup---------->
+<div id="popup-ahmed-body">
+  <img id ="close" src="techno tour website design pro/exit.png" alt="">
+  <center><div class="popup-container">
+    <span id="image"><img src="techno tour website design pro/adel.jpg" alt="adel"></span>
+    <h2 id="popup-name">Ahmed Hosny</h2>
+    <!-----------Skills----------->
+    <span id="popup-skills">
+      <span id="popup-title-Skills">Skills</span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Html :</span> 
+        <span id="popup-subject-body"><span id="popup-ahmed-html">100%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Css :</span> 
+        <span id="popup-subject-body"><span id="popup-ahmed-css">90%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">C++ :</span> 
+        <span id="popup-subject-body"><span id="popup-ahmed-c">70%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">JS :</span> 
+        <span id="popup-subject-body"><span id="popup-ahmed-js">60%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Python :</span> 
+        <span id="popup-subject-body"><span id="popup-ahmed-python">100%</span></span>
+      </span>
+    </span>
+    <!-----------Contact----------->
+    <span id="popup-contact">
+      <span id="popup-contact-title">Contact Me</span> 
+      <span id="popup-contact-row">
+        <span id="popup-ahmed-whatsapp"><img src="techno tour website design pro/whatsapp.png" alt="">Whatsapp: </span>
+        <p><a href="https://whatsapp.com/dl/">01283048731</a></p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-ahmed-facebook"><img src="techno tour website design pro/facebook.png" alt="">Facebook: </span>
+        <p><a href="https://www.facebook.com/profile.php?id=100008321981890">Adel Sherif</a> </p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-ahmed-phone"><img src="techno tour website design pro/phone.png" alt="">Phone: </span>
+        <p>01283048731</p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-ahmed-gmail"><img src="techno tour website design pro/gmail.png" alt="">Gmail: </span>
+        <p><a href="https://mail.google.com/mail/u/0/#inbox">Os6225091@Gmail.com</a> </p>
+      </span>
+    </span>
+  </div></center>
+</div>
+<!------------ End Ahmed popup---------->
+
+<!------------ Start Sara popup---------->
+<div id="popup-sara-body">
+  <img id ="close" src="techno tour website design pro/exit.png" alt="">
+  <center><div class="popup-container">
+    <span id="image"><img src="techno tour website design pro/adel.jpg" alt="adel"></span>
+    <h2 id="popup-name">Sara Magdy</h2>
+    <!-----------Skills----------->
+    <span id="popup-skills">
+      <span id="popup-title-Skills">Skills</span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Html :</span> 
+        <span id="popup-subject-body"><span id="popup-sara-html">100%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Css :</span> 
+        <span id="popup-subject-body"><span id="popup-sara-css">90%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">C++ :</span> 
+        <span id="popup-subject-body"><span id="popup-sara-c">70%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">JS :</span> 
+        <span id="popup-subject-body"><span id="popup-sara-js">60%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Python :</span> 
+        <span id="popup-subject-body"><span id="popup-sara-python">100%</span></span>
+      </span>
+    </span>
+    <!-----------Contact----------->
+    <span id="popup-contact">
+      <span id="popup-contact-title">Contact Me</span> 
+      <span id="popup-contact-row">
+        <span id="popup-sara-whatsapp"><img src="techno tour website design pro/whatsapp.png" alt="">Whatsapp: </span>
+        <p><a href="https://whatsapp.com/dl/">01283048731</a></p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-sara-facebook"><img src="techno tour website design pro/facebook.png" alt="">Facebook: </span>
+        <p><a href="https://www.facebook.com/profile.php?id=100008321981890">Adel Sherif</a> </p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-amr-phone"><img src="techno tour website design pro/phone.png" alt="">Phone: </span>
+        <p>01283048731</p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-sara-gmail"><img src="techno tour website design pro/gmail.png" alt="">Gmail: </span>
+        <p><a href="https://mail.google.com/mail/u/0/#inbox">Os6225091@Gmail.com</a> </p>
+      </span>
+    </span>
+  </div></center>
+</div>
+<!------------ End Sara popup---------->
+
+<!------------ Start Shahd popup---------->
+<div id="popup-shahd-body">
+  <img id ="close" src="techno tour website design pro/exit.png" alt="">
+  <center><div class="popup-container">
+    <span id="image"><img src="techno tour website design pro/adel.jpg" alt="adel"></span>
+    <h2 id="popup-name">Shahd Abo Elkaramat</h2>
+    <!-----------Skills----------->
+    <span id="popup-skills">
+      <span id="popup-title-Skills">Skills</span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Html :</span> 
+        <span id="popup-subject-body"><span id="popup-shahd-html">100%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Css :</span> 
+        <span id="popup-subject-body"><span id="popup-shahd-css">90%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">C++ :</span> 
+        <span id="popup-subject-body"><span id="popup-shahd-c">70%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">JS :</span> 
+        <span id="popup-subject-body"><span id="popup-shahd-js">60%</span></span>
+      </span>
+      <span id="popup-subject-row">
+        <span id="popup-subject-name">Python :</span> 
+        <span id="popup-subject-body"><span id="popup-shahd-python">100%</span></span>
+      </span>
+    </span>
+    <!-----------Contact----------->
+    <span id="popup-contact">
+      <span id="popup-contact-title">Contact Me</span> 
+      <span id="popup-contact-row">
+        <span id="popup-shahd-whatsapp"><img src="techno tour website design pro/whatsapp.png" alt="">Whatsapp: </span>
+        <p><a href="https://whatsapp.com/dl/">01283048731</a></p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-shahd-facebook"><img src="techno tour website design pro/facebook.png" alt="">Facebook: </span>
+        <p><a href="https://www.facebook.com/profile.php?id=100008321981890">Adel Sherif</a> </p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-shahd-phone"><img src="techno tour website design pro/phone.png" alt="">Phone: </span>
+        <p>01283048731</p>
+      </span>
+      <span id="popup-contact-row">
+        <span id="popup-shahd-gmail"><img src="techno tour website design pro/gmail.png" alt="">Gmail: </span>
+        <p><a href="https://mail.google.com/mail/u/0/#inbox">Os6225091@Gmail.com</a> </p>
+      </span>
+    </span>
+  </div></center>
+</div>
+<!------------ End Shahd popup---------->
+
+                <!-------------------------------End pop up pages----------------------------->
+
+</section>
+<!---End The Main Section Have All Page--->
 </body>
+
+<!-------------------------------Start  pop up pages Js----------------------------->
+<script>
+  /*************Adel And cancelBtn Js********/
+    const   section1 = document.querySelector("section"),
+            adel = section1.querySelector("#adel"),
+           cancelBtn = document.querySelectorAll("#close");
+        
+
+        adel.addEventListener("click", ()=>{
+            section1.classList.add("show-adel");
+        });
+        cancelBtn.forEach(cBtn => {
+            cBtn.addEventListener("click", () =>{
+                section1.classList.remove("show-adel");
+            });
+        });
+    /*************Amr Js***************/
+        section2 = document.querySelector("section"),
+            amr = section2.querySelector("#amr"),
+
+        amr.addEventListener("click", ()=>{
+            section2.classList.add("show-amr");
+        });
+        cancelBtn.forEach(cBtn => {
+            cBtn.addEventListener("click", () =>{
+                section2.classList.remove("show-amr");
+            });
+        });
+    /*************Ahmed Js***************/
+        section3 = document.querySelector("section"),
+            ahmed = section3.querySelector("#ahmed"),
+
+        ahmed.addEventListener("click", ()=>{
+          section3.classList.add("show-ahmed");
+        });
+        cancelBtn.forEach(cBtn => {
+            cBtn.addEventListener("click", () =>{
+              section3.classList.remove("show-ahmed");
+            });
+        });
+            /*************Sara Js***************/
+            section4 = document.querySelector("section"),
+            sara = section4.querySelector("#sara"),
+
+            sara.addEventListener("click", ()=>{
+          section4.classList.add("show-sara");
+        });
+        cancelBtn.forEach(cBtn => {
+            cBtn.addEventListener("click", () =>{
+              section4.classList.remove("show-sara");
+            });
+        });
+            /*************Shahd Js***************/
+            section5 = document.querySelector("section"),
+            shahd = section5.querySelector("#shahd"),
+
+            shahd.addEventListener("click", ()=>{
+              section5.classList.add("show-shahd");
+        });
+        cancelBtn.forEach(cBtn => {
+            cBtn.addEventListener("click", () =>{
+              section5.classList.remove("show-shahd");
+            });
+        });
+</script>
+<!-------------------------------End pop up pages Js----------------------------->
+
 </html>
--- database.php
+++ database.php
@@ -7,12 +7,12 @@
 //charset=utf8
 //name = root
 //password=""
-$conn = new PDO("mysql:host=localhost;dbname=technotour;charset=utf8;", "root","");
-// Check connection
-if ($conn) {
-    echo "";
-}else{
-    echo "not connect";
-}
+// $conn = new PDO("mysql:host=localhost;dbname=technotour;charset=utf8;", "root","");
+// // Check connection
+// if ($conn) {
+//     echo "";
+// }else{
+//     echo "not connect";
+// }
 
 ?>
