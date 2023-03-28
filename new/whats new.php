e281e624f5a211e32a180d3012be6bebdcadfd82
saramagdyy <saramagdyy77@gmail.com>
2023-03-28 21:44:19 +0200

    Create whats new.php

---
+++ new/whats new.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>What's New</title>
+</head>
+<body>
+    <center><h1 id="user"></h1></center>
+    <center><h1>What's new are Coming Soon</h1></center>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+        }
+        body{
+            background-image: url('techno tour website design pro/singin.png');
+            width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    position:initial;
+    mask-repeat: initial;
+    background-attachment: fixed;
+
+        }
+        h1{
+            color: aqua;
+            margin-top: 11%;
+        }
+    </style>
+</body>
+
+</html>
