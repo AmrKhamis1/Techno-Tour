ec9db4ccb1b137a1a47dbddcc584c7204a801c61
Amr Khamis <101368138+AmrKhamis1@users.noreply.github.com>
2023-04-17 09:33:55 +0200

    up

--- new/form/verification.php
+++ new/form/verification.php
@@ -13,11 +13,11 @@ if(empty($fname) || empty($lname)||empty($email)||empty($password) || empty($v))
 if(isset($_POST['v-submit'])){
 
    if( password_verify($_POST['v_number'],$v)){
-    include "./database/databasemysqli.php";
+include "../database/databasemysqli.php";
 $insert=$connection->prepare("INSERT INTO members(fname,lname,email,password,position)
 VALUES ('$fname','$lname','$email','".$_GET['password']."','$position')");
 $insert->execute();
-header('Location:./form.php');
+header('Location:../form.php');
    }else{
     $ema_pass='code not correct !';
    }
@@ -26,20 +26,28 @@ header('Location:./form.php');
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./CSS/form.css"> 
-    <script src="./JS/form.js"></script>
+    <link rel="stylesheet" href="../CSS/form.css"> 
+    <script src="../JS/form.js"></script>
     <title>verify</title>
 </head>
 
 <body>
     <div id="container">
     </div>
-    <form id="form11" action="" method="post" novalidate>
-        <img src="./techno tour website design pro/login.png" width="50px" alt="login.png">
+    <form id="form11" action="" method="post">
+        <img src="../techno tour website design pro/login.png" width="50px" alt="login.png">
         <h1>Verify Code </h1>
-       <p> <?php if(isset($ema_pass)){ echo $ema_pass;}?></p>
+       <p style='color:red;'> <?php if(isset($ema_pass)){ echo $ema_pass;}?></p>
         <br>
-        <label for="remember" style='font-size:10px;'>Enter The code</label>
+        <?php
+        if($position=='Dr'||$position=='Assisstant'){
+               echo "
+               <label for='vdr_number' style='font-size:10px;'>Enter uni code for ".$position."s</label>
+               <input id='vdr_number' name='vdr_number' type='number' class='input-text input-text1' required/>
+               ";
+        }
+        ?>
+        <label for="v_number" style='font-size:10px;'>Enter The verification code</label>
         <input id="v_number" name="v_number" type="number" class="input-text input-text1" required/>
         <button class="buttons1" type="submit" name="v-submit">done</button>
     </form>
